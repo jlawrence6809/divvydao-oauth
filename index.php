e3bc66f18da9e7a20e5663cdb0dfd3a77456d87f
j <jlawrence6809@yahoo.com>
2018-01-28 21:39:54 -0800

    test

--- index.php
+++ index.php
@@ -8,19 +8,10 @@ ini_set('display_errors', 'On');
 session_start();
 
 print('reaching index.php');
+print('current page: '.currentPage());
 
-if(currentPage() == 'authorize'){
-	pageContent();
-}else{
-    // $request = getProvider()->getAuthenticatedRequest(
-    //     'GET',
-    //     'https://chat.divvydao.net/api/v4/users',
-    //     $_SESSION['accessToken']
-    // );
-    // $response = getProvider()->getResponse($request);
-    // var_export($response->getBody()->getContents());
-
-    // var_export(makeMattermostRequest('api/v4/users'));
-
-	run();
-}
+// if(currentPage() == 'authorize'){
+// 	pageContent();
+// }else{
+// 	run();
+// }
