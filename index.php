f08c4076411cf679bd746363ad35f8fbeefe0088
j <jlawrence6809@yahoo.com>
2018-01-28 21:40:48 -0800

    test

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ require_once 'functions.php';
 ini_set('display_errors', 'On');
 session_start();
 
-print('reaching index.php');
+print('reaching index.php\n');
 print('current page: '.currentPage());
+
+if(currentPage() == 'home'){
+    run();
+}
 
 // if(currentPage() == 'authorize'){
 // 	pageContent();
